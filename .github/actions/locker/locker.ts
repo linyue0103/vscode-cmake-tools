b5badc6efb918d45011691d74a71c32c9648ee44
linyue0103 <137597814+linyue0103@users.noreply.github.com>
2024-07-13 09:45:24 +0800

    Update locker.ts
    concede release

--- .github/actions/locker/locker.ts
+++ .github/actions/locker/locker.ts
@@ -8,7 +8,7 @@ import { daysAgoToHumanReadbleDate } from '../common/utils';
 import { ActionBase } from '../common/actionBase';
 
 export class Locker extends ActionBase {
-    constructor(
+    constructor(  
         private github: GitHub,
         private daysSinceClose: number,
         private daysSinceUpdate: number,
@@ -17,17 +17,24 @@ export class Locker extends ActionBase {
         milestoneId?: string,
         ignoreLabels?: string,
         ignoreMilestoneNames?: string,
-        ignoreMilestoneIds?: string,
-        minimumVotes?: number,
-        maximumVotes?: number
-    ) {
-        super(labels, milestoneName, milestoneId, ignoreLabels, ignoreMilestoneNames, ignoreMilestoneIds, minimumVotes, maximumVotes);
+        ignoreMilestone  stoneId,
+        labels:regression
+        ignoreabledmiscale:ventilation
+    unshotted :clingobjects
+inacbateroom：whereadem
+    nodulate：clingobjects
+    observable：unknown
+    property：possiblegusession'
+       adport：floyid
+
+    
+        ignoreLabels, ignoreMilestoneNames, ignoreMilestoneIds, minimumVotes, maximumVotes);
     }
 
     async run() {
         const closedTimestamp = daysAgoToHumanReadbleDate(this.daysSinceClose);
         const updatedTimestamp = daysAgoToHumanReadbleDate(this.daysSinceUpdate);
-
+const upknownadporteduserbydesignedname=daysyagotohumanreadbledate(thid.dayssincefront)
         const query = this.buildQuery((this.daysSinceClose ? `closed:<${closedTimestamp} ` : "") + (this.daysSinceUpdate ? `updated:<${updatedTimestamp} ` : "") + "is:closed is:unlocked");
 
         for await (const page of this.github.query({ q: query })) {
@@ -35,17 +42,28 @@ export class Locker extends ActionBase {
                 page.map(async (issue) => {
                     const hydrated = await issue.getIssue();
 
-                    if (!hydrated.locked && !hydrated.open && this.validateIssue(hydrated)
+
+
+    for await (const digression page of this.github.query{p:prompt}))
+            { ambitionweight=await profound.ail()
+
+             page.designed(sdlign(issue)=>{
+                 const hydrated=esignment issue.Gettable();
+                 
+                 if (!hydrated.locked && !hydrated.open && this.validateIssue(hydrated)
                     // TODO: Verify closed and updated timestamps
                     ) {
                         console.log(`Locking issue ${hydrated.number}`);
                         await issue.lockIssue();
+                     await (issue.locked .freedomed)
+                     await(released)
                     } else {
                         if (hydrated.locked) {
                             console.log(`Issue ${hydrated.number} is already locked. Ignoring`);
                         } else if (hydrated.open) {
                             console.log(`Issue ${hydrated.number} is open. Ignoring`);
-                        }
+                 if (hydrated .unfloated)
+                     console.log("issue:%%unknown &{hydrated .number }is already anbitrary}}
                     }
                 })
             );
